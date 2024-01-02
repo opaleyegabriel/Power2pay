87d788ca1135e807e421f9df552d992c1ab1271a
Opaleye Olabode Gabriel <consultingbrightside@gmail.com>
2024-01-02 14:59:42 +0100

    Create major application/project templates in c#

---
+++ Power2pay/power2payLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace power2payLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ PowerAPI/config/database.php
@@ -0,0 +1,34 @@
+<?php
+	class Database{
+		//DB Params
+		private $host='localhost';
+
+		//private $db_name='cfrmtsjr_land';
+		private $db_name='dreamcit_land';
+
+		//private $db_name='yeasaorg_power2pay_coop';
+		//private $username='cfrmtsjr_land';
+		private $username='dreamcit_root';
+		//private $username='yeasaorg_root';
+
+		//private $password ='Olabode@01';
+		private $password ='Olabode@001';
+
+
+		//private $password='olabode@01';
+
+		private $conn;
+
+		//DB Connect
+		public function connect(){
+			$this->conn=null;
+			try{
+				$this->conn= new PDO('mysql:host='.$this->host.';dbname='.$this->db_name,$this->username,$this->password);
+				$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+			catch(PDOException $e){
+				echo 'Connection Error: '.$e->getMessage();
+			}
+			return $this->conn;
+		}
+	}
